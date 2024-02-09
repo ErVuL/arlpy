faeb88b88eca3fe1ef3564836a066e4157556c2a
ErVuL <theo.bertet@gmail.com>
2024-02-09 19:44:10 +0100

    Beginning a new class for Bellhop.

--- arlpy/uwapm.py
+++ arlpy/uwapm.py
@@ -90,6 +90,7 @@ def create_env2d(**kv):
         'name': 'arlpy',
         'type': '2D',                    # 2D only
         'model': 'BELLHOP',              # Model: BELLHOP, KRAKEN, RAM
+        'mode': incoherent,
         'frequency': 25000,              # Source frequency in Hz
         'soundspeed': 1500,              # m/s
         'soundspeed_range': [0],         # m
@@ -1110,6 +1111,391 @@ class _Bellhop:
         self.bts = _np.mean(env['bottom_soundspeed'])
         self.btd = _np.mean(env['bottom_density'])
         self.bta = _np.mean(env['bottom_absorption'])
+        self._print(fh, "%0.6f %0.6f 0.0 %0.6f %0.6f /" % (max_depth, self.bts, self.btd/1000, self.bta))
+        self._print_array(fh, env['tx_depth'])
+        self._print_array(fh, env['rx_depth'])
+        self._print_array(fh, env['rx_range']/1000)
+        if env['tx_directionality'] is None:
+            self._print(fh, "'"+taskcode+"'")
+        else:
+            self._print(fh, "'"+taskcode+" *'")
+            self._create_sbp_file(fname_base+'.sbp', env['tx_directionality'])
+        self._print(fh, "%d" % (env['nbeams']))
+        self._print(fh, "%0.6f %0.6f /" % (env['min_angle'], env['max_angle']))
+        self._print(fh, "0.0 %0.6f %0.6f" % (1.01*max_depth, 1.01*_np.max(env['rx_range'])/1000))
+        _os.close(fh)
+        
+        return fname_base
+
+    def _create_bty_ati_file(self, filename, depth, interp):
+        with open(filename, 'wt') as f:
+            f.write("'%c'\n" % ('C' if interp == curvilinear else 'L'))
+            f.write(str(depth.shape[0])+"\n")
+            for j in range(depth.shape[0]):
+                f.write("%0.6f %0.6f\n" % (depth[j,0]/1000, depth[j,1]))
+
+    def _create_sbp_file(self, filename, dir):
+        with open(filename, 'wt') as f:
+            f.write(str(dir.shape[0])+"\n")
+            for j in range(dir.shape[0]):
+                f.write("%0.6f %0.6f\n" % (dir[j,0], dir[j,1]))
+
+    def _create_ssp_file(self, filename, svp):
+        with open(filename, 'wt') as f:
+            f.write(str(svp.shape[1])+"\n")
+            for j in range(svp.shape[1]):
+                f.write("%0.6f%c" % (svp.columns[j]/1000, '\n' if j == svp.shape[1]-1 else ' '))
+            for k in range(svp.shape[0]):
+                for j in range(svp.shape[1]):
+                    f.write("%0.6f%c" % (svp.iloc[k,j], '\n' if j == svp.shape[1]-1 else ' '))
+
+    def _readf(self, f, types, dtype=str):
+        if type(f) is str:
+            p = _re.split(r' +', f.strip())
+        else:
+            p = _re.split(r' +', f.readline().strip())
+        for j in range(len(p)):
+            if len(types) > j:
+                p[j] = types[j](p[j])
+            else:
+                p[j] = dtype(p[j])
+        return tuple(p)
+
+    def _check_error(self, fname_base):
+        err = None
+        try:
+            with open(fname_base+'.prt', 'rt') as f:
+                for lno, s in enumerate(f):
+                    if err is not None and s != '\n':
+                        err += '[ERROR] BELLHOP:' + s[:-1] + '.\n'
+                    elif '*** FATAL ERROR ***' in s:
+                        err = '[ERROR] BELLHOP:' + s
+        except:
+            pass
+        return err
+
+    def _load_arrivals(self, fname_base):
+        with open(fname_base+'.arr', 'rt') as f:
+            hdr = f.readline()
+            if hdr.find('2D') >= 0:
+                freq = self._readf(f, (float,))
+                tx_depth_info = self._readf(f, (int,), float)
+                tx_depth_count = tx_depth_info[0]
+                tx_depth = tx_depth_info[1:]
+                assert tx_depth_count == len(tx_depth)
+                rx_depth_info = self._readf(f, (int,), float)
+                rx_depth_count = rx_depth_info[0]
+                rx_depth = rx_depth_info[1:]
+                assert rx_depth_count == len(rx_depth)
+                rx_range_info = self._readf(f, (int,), float)
+                rx_range_count = rx_range_info[0]
+                rx_range = rx_range_info[1:]
+                assert rx_range_count == len(rx_range)
+            else:
+                freq, tx_depth_count, rx_depth_count, rx_range_count = self._readf(hdr, (float, int, int, int))
+                tx_depth = self._readf(f, (float,)*tx_depth_count)
+                rx_depth = self._readf(f, (float,)*rx_depth_count)
+                rx_range = self._readf(f, (float,)*rx_range_count)
+            arrivals = []
+            for j in range(tx_depth_count):
+                f.readline()
+                for k in range(rx_depth_count):
+                    for m in range(rx_range_count):
+                        count = int(f.readline())
+                        for n in range(count):
+                            data = self._readf(f, (float, float, float, float, float, float, int, int))
+                            arrivals.append(_pd.DataFrame({
+                                'tx_depth_ndx': [j],
+                                'rx_depth_ndx': [k],
+                                'rx_range_ndx': [m],
+                                'tx_depth': [tx_depth[j]],
+                                'rx_depth': [rx_depth[k]],
+                                'rx_range': [rx_range[m]],
+                                'arrival_number': [n],
+                                # 'arrival_amplitude': [data[0]*_np.exp(1j * data[1]* _np.pi/180)],
+                                'arrival_amplitude': [data[0] * _np.exp( -1j * (_np.deg2rad(data[1]) + freq[0] * 2 * _np.pi * (data[3] * 1j +  data[2])))],
+                                'time_of_arrival': [data[2]],
+                                'complex_time_of_arrival': [data[2] + 1j*data[3]],
+                                'angle_of_departure': [data[4]],
+                                'angle_of_arrival': [data[5]],
+                                'surface_bounces': [data[6]],
+                                'bottom_bounces': [data[7]]
+                            }, index=[len(arrivals)+1]))
+        return _pd.concat(arrivals)
+
+    def _load_rays(self, fname_base):
+        with open(fname_base+'.ray', 'rt') as f:
+            f.readline()
+            f.readline()
+            f.readline()
+            f.readline()
+            f.readline()
+            f.readline()
+            f.readline()
+            rays = []
+            while True:
+                s = f.readline()
+                if s is None or len(s.strip()) == 0:
+                    break
+                a = float(s)
+                pts, sb, bb = self._readf(f, (int, int, int))
+                ray = _np.empty((pts, 2))
+                for k in range(pts):
+                    ray[k,:] = self._readf(f, (float, float))
+                rays.append(_pd.DataFrame({
+                    'angle_of_departure': [a],
+                    'surface_bounces': [sb],
+                    'bottom_bounces': [bb],
+                    'ray': [ray]
+                }))
+        return _pd.concat(rays)
+
+    def _load_shd(self, fname_base):
+        with open(fname_base+'.shd', 'rb') as f:
+            recl, = _unpack('i', f.read(4))
+            title = str(f.read(80))
+            f.seek(4*recl, 0)
+            ptype = f.read(10).decode('utf8').strip()
+            assert ptype == 'rectilin', 'Invalid file format (expecting ptype == "rectilin")'
+            f.seek(8*recl, 0)
+            nfreq, ntheta, nsx, nsy, nsd, nrd, nrr, atten = _unpack('iiiiiiif', f.read(32))
+            assert nfreq == 1, 'Invalid file format (expecting nfreq == 1)'
+            assert ntheta == 1, 'Invalid file format (expecting ntheta == 1)'
+            assert nsd == 1, 'Invalid file format (expecting nsd == 1)'
+            f.seek(32*recl, 0)
+            pos_r_depth = _unpack('f'*nrd, f.read(4*nrd))
+            f.seek(36*recl, 0)
+            pos_r_range = _unpack('f'*nrr, f.read(4*nrr))
+            pressure = _np.zeros((nrd, nrr), dtype=_np.complex128)
+            for ird in range(nrd):
+                recnum = 10 + ird
+                f.seek(recnum*4*recl, 0)
+                temp = _np.array(_unpack('f'*2*nrr, f.read(2*nrr*4)))
+                pressure[ird,:] = temp[::2] + 1j*temp[1::2]
+        return _pd.DataFrame(pressure, index=pos_r_depth, columns=pos_r_range)
+    
+_models.append(('BELLHOP', _Bellhop))
+
+
+class bellhop:
+
+    def __init__(self):
+        
+        self.env               = None
+        self.transmission_loss = None 
+        self.eigen_rays        = None
+        self.arrivals          = None
+        self.impulse_response  = None
+
+    def create_env(self, env, copy=False):
+        
+        # Assert environment
+        assert self.check_env(env), "Bellhop environment not correctly defined !"
+
+        # Define self environment by copy or reference
+        if copy:
+            self.env = env.copy()
+        else:
+            self.env = env
+        return True
+        
+    def check_env(self, env):
+        # @todo
+       
+        return True
+
+    def compute_tranmission_loss(self, debug=False, **kwargs):
+        
+        # Assert environment
+        assert self.check_env(self.env), "Bellhop environment not correctly defined !"
+        
+        # Define mode taskcode
+        if self.env['mode'] == coherent:
+            taskcode = 'C'
+        elif self.env['mode'] == incoherent:
+            taskcode = 'I'
+        elif self.env['mode'] == semicoherent:
+            taskcode = 'S'
+        else:
+            raise Exception("Bellhop mode not known !")
+           
+        # Generate temporary env file and get base name used for all temporary files
+        fname_base = self._create_env_file(self.env, taskcode)
+        
+        # Debug print
+        if debug:
+            print('[DBG] Bellhop working files: '+fname_base+'.')
+        
+        # Compute TL
+        if self._bellhop(fname_base):
+            err = self._check_error(fname_base)
+            if err is not None:
+                raise RuntimeError(err) 
+            else:
+                try:
+                    self.transmission_loss = self._load_shd(fname_base)
+                except FileNotFoundError:
+                    raise FileNotFoundError('Bellhop fortran execution did not generate expected output file !')
+        
+        # Delete temporary generated files
+        self._unlink_all(fname_base)     
+        
+        return self.transmission_loss
+    
+    def plot_transmission_loss(self, tloss=None, env=None, **kwargs):
+        
+        if tloss == None:
+            tloss = self.transmission_loss
+
+        if env == None:
+            env = self.env        
+        
+        # @todo
+        
+    
+    def supports(self, env=None, task=None):
+        if env is not None and env['type'] != '2D':
+            return False
+        fh, fname = _mkstemp(suffix='.env')
+        _os.close(fh)
+        fname_base = fname[:-4]
+        self._unlink(fname_base+'.env')
+        rv = self._bellhop(fname_base)
+        self._unlink(fname_base+'.prt')
+        self._unlink(fname_base+'.log')
+        return rv
+
+    def run(self, env, task, debug=False):
+        taskmap = {
+            arrivals:     ['A', self._load_arrivals],
+            eigenrays:    ['E', self._load_rays],
+            rays:         ['R', self._load_rays],
+            coherent:     ['C', self._load_shd],
+            incoherent:   ['I', self._load_shd],
+            semicoherent: ['S', self._load_shd]
+        }
+        fname_base = self._create_env_file(env, taskmap[task][0])
+        results = None
+        if self._bellhop(fname_base):
+            err = self._check_error(fname_base)
+            if err is not None:
+                print(err)
+            else:
+                try:
+                    results = taskmap[task][1](fname_base)
+                except FileNotFoundError:
+                    print('[WARN] Bellhop did not generate expected output file')
+        if debug:
+            print('[DEBUG] Bellhop working files: '+fname_base+'.*.')
+        else:
+            self._unlink(fname_base+'.env')
+            self._unlink(fname_base+'.bty')
+            self._unlink(fname_base+'.ssp')
+            self._unlink(fname_base+'.ati')
+            self._unlink(fname_base+'.sbp')
+            self._unlink(fname_base+'.prt')
+            self._unlink(fname_base+'.log')
+            self._unlink(fname_base+'.arr')
+            self._unlink(fname_base+'.ray')
+            self._unlink(fname_base+'.shd')
+        return results
+
+    def _bellhop(self, *args):
+        try:
+            _proc.run(f'bellhop.exe {" ".join(list(args))}', 
+                      stderr=_proc.STDOUT, stdout=_proc.PIPE,
+                      shell=True)
+        except OSError:
+            return False
+        return True
+
+    def _unlink(self, f):
+        try:
+            _os.unlink(f)
+        except:
+            pass
+        
+    def _unlink_all(self, fname_base):
+        
+            self._unlink(fname_base+'.env')
+            self._unlink(fname_base+'.bty')
+            self._unlink(fname_base+'.ssp')
+            self._unlink(fname_base+'.ati')
+            self._unlink(fname_base+'.sbp')
+            self._unlink(fname_base+'.prt')
+            self._unlink(fname_base+'.log')
+            self._unlink(fname_base+'.arr')
+            self._unlink(fname_base+'.ray')
+            self._unlink(fname_base+'.shd')
+
+    def _print(self, fh, s, newline=True):
+        _os.write(fh, (s+'\n' if newline else s).encode())
+
+    def _print_array(self, fh, a):
+        if _np.size(a) == 1:
+            self._print(fh, "1")
+            self._print(fh, "%0.6f /" % (a))
+        else:
+            self._print(fh, str(_np.size(a)))
+            for j in a:
+                self._print(fh, "%0.6f " % (j), newline=False)
+            self._print(fh, "/")
+
+    def _create_env_file(self, env, taskcode):
+        # @todo     Bottom absorption or attenuation ?
+        # @todo     Roughness interface or roughness ?
+        fh, fname = _mkstemp(suffix='.env')
+        fname_base = fname[:-4]
+        self._print(fh, "'"+env['name']+"'")
+        self._print(fh, "%0.6f" % (env['frequency']))
+        self._print(fh, "1")
+        if _np.size(env['soundspeed'],axis=1) > 1:
+            print(f"[INFO] {env['model']}: Multiple sound profiles not supported, using average value.")
+            mn = _np.mean(env['soundspeed'], axis=1)
+            svp = _np.column_stack((env['soundspeed_depth'], mn))
+        else:
+            svp = _np.column_stack((env['soundspeed_depth'], env['soundspeed']))
+        svp_depth = 0.0
+        svp_interp = 'S' if env['soundspeed_interp'] == spline else 'C'
+        if isinstance(svp, _pd.DataFrame):
+            svp_depth = svp.index[-1]
+            if len(svp.columns) > 1:
+                svp_interp = 'Q'
+            else:
+                svp = _np.hstack((_np.array([svp.index]).T, _np.asarray(svp)))
+        if env['surface'] is None:
+            self._print(fh, "'%cVWT'" % svp_interp)
+        else:
+            self._print(fh, "'%cVWT*'" % svp_interp)
+            self._create_bty_ati_file(fname_base+'.ati', env['surface'], env['surface_interp'])
+        #max depth should be the depth of the acoustic domain, which can be deeper than the max depth bathymetry
+        max_depth = env['depth'] if _np.size(env['depth']) == 1 else max(_np.max(env['depth'][:,1]), svp_depth)
+        self._print(fh, "1 0.0 %0.6f" % (max_depth))
+        if _np.size(svp) == 1:
+            self._print(fh, "0.0 %0.6f /" % (svp))
+            self._print(fh, "%0.6f %0.6f /" % (max_depth, svp))
+        elif svp_interp == 'Q':
+            for j in range(svp.shape[0]):
+                self._print(fh, "%0.6f %0.6f /" % (svp.index[j], svp.iloc[j,0]))
+            self._create_ssp_file(fname_base+'.ssp', svp)
+        else:
+            for j in range(svp.shape[0]):
+                self._print(fh, "%0.6f %0.6f /" % (svp[j,0], svp[j,1]))
+        depth = env['depth']
+        if _np.size(depth) == 1:
+            self._print(fh, "'A' %0.6f" % (env['bottom_roughness_interface']))
+        else:
+            self._print(fh, "'A*' %0.6f" % (env['bottom_roughness_interface']))
+            self._create_bty_ati_file(fname_base+'.bty', depth, env['depth_interp'])  
+        if env['bottom_soundspeed'].ndim > 0:
+            print(f"[INFO] {env['model']}: Multiple bottom soundspeed profiles not supported, using average value.")
+        if env['bottom_density'].ndim > 0:
+            print(f"[INFO] {env['model']}: Multiple bottom density profiles not supported, using average value.")
+        if env['bottom_attenuation'].ndim > 0:
+            print(f"[INFO] {env['model']}: Multiple bottom absorption profiles not supported, using average value.")
+        self.bts = _np.mean(env['bottom_soundspeed'])
+        self.btd = _np.mean(env['bottom_density'])
+        self.bta = _np.mean(env['bottom_attenuation'])
         self._print(fh, "%0.6f %0.6f 0.0 %0.6f %0.6f /" % (max_depth, self.bts, self.btd/1000, self.bta))
         self._print_array(fh, env['tx_depth'])
         self._print_array(fh, env['rx_depth'])
