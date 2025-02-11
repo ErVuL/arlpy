148ce7d22c3a1c78f00b4e3119d86e54f99c3a50
ErVuL <theo.bertet@gmail.com>
2025-02-11 20:33:27 +0100

    Small example update

--- examples/noise_synthesis.py
+++ examples/noise_synthesis.py
@@ -5,8 +5,8 @@ import arlpy.uwa as uwa
 if __name__ == "__main__":
 
     # PSD of required signal
-    wenz = uwa.WenzModel(Fxx=np.linspace(1,192000,65536))
-    duration = 10 # s
+    wenz = uwa.WenzModel(Fxx=np.linspace(1,192000,65536), shipping_level='medium', wind_speed=10)
+    duration = 60 # s
 
     # Signal synthesis
     Gtime, Gsignal, fs = uwa.SSRP(wenz.Pxx, wenz.Fxx, duration, scale=1)
@@ -16,13 +16,13 @@ if __name__ == "__main__":
     Fxx, Pxx = psd.compute(Gsignal, fs)
     
     # Plot
-    fig, ax = psd.plot(title='Example', label='Generated signal', color='black')
+    fig, ax = psd.plot(title='Wenz noise synthesis', label='Generated signal', color='black')
     psd.add2plot(ax=ax, Fxx=wenz.Fxx, Pxx=wenz.Pxx, label='Goal', linestyle='dashed', color='red')
     fig1, ax1 =  plt.subplots(1,1)
     ax1.plot(Gtime,Gsignal)
     ax1.set_xlabel('Time [s]')
     ax1.set_ylabel('Amplitude [Pa]')
-    ax1.set_title('Generated signal', loc='left')
+    ax1.set_title('[Generated signal] Wenz noise', loc='left')
     ax1.grid(True, which="both")
     wenz.plot()
         
